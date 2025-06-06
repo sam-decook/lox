2a4266f4dbd48af8cb83d485bfe82a2b71bf44e0
Sam DeCook <samuel.decook@gmail.com>
2025-06-05 19:25:37 -0600

    19 - Strings

--- clox/compiler.c
+++ clox/compiler.c
@@ -166,6 +166,12 @@ static void number() {
     emitConstant(NUMBER_VAL(value));
 }
 
+static void string() {
+    Token prev = parser.previous;
+    // Trim the quotation marks
+    emitConstant(OBJ_VAL(copyString(prev.start + 1, prev.length - 2)));
+}
+
 static void unary() {
     TokenType operatorType = parser.previous.type;
 
@@ -232,7 +238,7 @@ ParseRule rules[] = {
   [TOKEN_LESS]          = {NULL,      binary,  PREC_COMPARISON},
   [TOKEN_LESS_EQUAL]    = {NULL,      binary,  PREC_COMPARISON},
   [TOKEN_IDENTIFIER]    = {NULL,      NULL,    PREC_NONE},
-  [TOKEN_STRING]        = {NULL,      NULL,    PREC_NONE},
+  [TOKEN_STRING]        = {string,      NULL,    PREC_NONE},
   [TOKEN_NUMBER]        = {number,    NULL,    PREC_NONE},
   [TOKEN_AND]           = {NULL,      NULL,    PREC_NONE},
   [TOKEN_CLASS]         = {NULL,      NULL,    PREC_NONE},
--- clox/memory.h
+++ clox/memory.h
@@ -2,10 +2,15 @@
 #define clox_memory_h
 
 #include "common.h"
+#include "object.h"
+
+#define ALLOCATE(type, count) (type*)reallocate(NULL, 0, sizeof(type) * count);
 
 // Start at 8 and double from there.
 #define GROW_CAPACITY(capacity) \
   ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
 
 // Wrapper for reallocate that gets the size of the type and returns a pointer of that type
 #define GROW_ARRAY(type, pointer, oldCount, newCount) \
@@ -15,5 +20,6 @@
   reallocate(pointer, sizeof(type) * (oldCount), 0)
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void freeObjects();
 
 #endif
